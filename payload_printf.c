ddea675dd6941eb76055ed338a9d57edae76d3c4
Simon Christmann <simon@christmann.email>
2021-03-17 08:53:41 +0100

    init
    Signed-off-by: Simon Christmann <simon@christmann.email>

---
+++ payload_printf.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "mosquitto_broker.h"
+#include "mosquitto_plugin.h"
+#include "mosquitto.h"
+#include "mqtt_protocol.h"
+
+#define UNUSED(A) (void)(A)
+
+static mosquitto_plugin_id_t *mosq_pid = NULL;
+
+static int callback_message(int event, void *event_data, void *userdata)
+{
+	struct mosquitto_evt_message *ed = event_data;
+
+	UNUSED(event);
+	UNUSED(userdata);
+
+	printf("payload: '%.*s'\n", ed->payloadlen, (char *)ed->payload);
+
+	return MOSQ_ERR_SUCCESS;
+}
+
+int mosquitto_plugin_version(int supported_version_count, const int *supported_versions)
+{
+	int i;
+
+	for(i=0; i<supported_version_count; i++){
+		if(supported_versions[i] == 5){
+			return 5;
+		}
+	}
+	return -1;
+}
+
+int mosquitto_plugin_init(mosquitto_plugin_id_t *identifier, void **user_data, struct mosquitto_opt *opts, int opt_count)
+{
+	UNUSED(user_data);
+	UNUSED(opts);
+	UNUSED(opt_count);
+
+	mosq_pid = identifier;
+	return mosquitto_callback_register(mosq_pid, MOSQ_EVT_MESSAGE, callback_message, NULL, NULL);
+}
+
+int mosquitto_plugin_cleanup(void *user_data, struct mosquitto_opt *opts, int opt_count)
+{
+	UNUSED(user_data);
+	UNUSED(opts);
+	UNUSED(opt_count);
+
+	return mosquitto_callback_unregister(mosq_pid, MOSQ_EVT_MESSAGE, callback_message, NULL);
+}
